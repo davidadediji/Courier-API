acd7cfe74dcabf2300469aa796d33f88da081a3a
davidadediji <davidadediji@gmail.com>
2022-11-05 19:47:57 +0100

    make new updates

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv')
 const calculation = require('./operation')
+
+dotenv.config()
+
+const port = process.env.PORT || 5000
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -11,6 +16,6 @@ app.post('/',calculation, (req, res) => {
 	res.send('Done')
 });
 
-app.listen(5005, () => {
-	console.log('listening');
+app.listen(port, () => {
+	console.log('listening on %d', port );
 });
