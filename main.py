eea8e286ff211714e27c325cd4982ead0680cd7c
davidadediji <davidadediji@gmail.com>
2023-09-25 00:39:18 +0100

    feat: instantiate gtk

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # weather app
+import gi
+import requests
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class ShowWeatherApp(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Weather App")
+        self.connect("destroy", Gtk.main_quit)
+
+    def fetch_weather_details(self):
+        requests.get();
+    def run(self):
+        self.show_all()
+        Gtk.main()
+
+
+if __name__ == "__main__":
+    app = ShowWeatherApp()
+    app.run()
